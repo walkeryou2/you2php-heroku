15db2203ae96546cfb15e0b8d9b4d4f7d865c876
walkeryou2 <36576157+walkeryou2@users.noreply.github.com>
2018-02-18 08:27:12 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyC7yi0jh6lgfpdyhsaeX2kJ4xprIDYBGro');//这里改成您的api key
-define('GJ_CODE', 'US');//国家代码
+define('GJ_CODE', 'TW');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF535e67CtzDSh4');//一个随机字符串，加解密KEY
